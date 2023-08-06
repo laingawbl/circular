83c79d83368609815ab2af8792be51f999eedd2d
taifun <laingawbl@gmail.com>
2023-08-06 16:55:59 -0700

    make the build script use cmake only

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -5,4 +5,4 @@ set -x
 rm -f cmake/SourceLists.cmake
 cp cmake/SourceLists{.official,}.cmake
 
-mkdir -p build/ && cd build && cmake .. && cmake --build . --target libcircular
+mkdir -p build/ && cd build && cmake .. && cmake --build . && ./tests/tests
